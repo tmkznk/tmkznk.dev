4803a21dc6b38fe47ee31b056644c58c19468e49
tmkznk <tomasz.zienkowicz@dietlabs.pl>
2026-06-23 21:25:28 +0200

    WIP: homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tmkznk.dev',
   vite: {
     plugins: [tailwindcss()]
   }
